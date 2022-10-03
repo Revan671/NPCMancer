2ae8d625af59dc5f118fa3c49be2d36efcc0bf48
Edwyn Ortiz <ESOrtiz@student.fullsail.edu>
2022-10-03 10:25:38 -0400

    UI for 3 Generate NPC screens (Placeholder images, text, buttons, layout).

---
+++ app/src/main/java/com/example/npcmancer/GenerateNPC1.java
@@ -0,0 +1,14 @@
+package com.example.npcmancer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateNPC1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_npc1);
+    }
+}
---
+++ app/src/main/java/com/example/npcmancer/GenerateNPC2.java
@@ -0,0 +1,14 @@
+package com.example.npcmancer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateNPC2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_npc2);
+    }
+}
